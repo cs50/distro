821fd11ee2e2a6f7a5123552436a249daa4011cc
Nate Hardison <natehardison@gmail.com>
2012-09-28 01:00:53 -0400

    Initial commit of pset2 staff solutions

---
+++ pset2/standard/caesar.c
@@ -0,0 +1,58 @@
+/**
+ * caesar.c
+ *
+ * Nate Hardison <nate@cs.harvard.edu>
+ *
+ * Encrypts user-supplied plaintext using a Caesar cipher.
+ */
+
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// there are 26 letters in the English alphabet!
+#define NUM_LETTERS 26
+
+int main(int argc, string argv[])
+{
+    // check for proper command line arguments
+    if (argc != 2)
+    {
+        printf("Usage: %s <key>\n", argv[0]);
+        return 1;
+    }
+
+    // ensure key is in range [0, inf)
+    int key = atoi(argv[1]);
+    if (key < 0)
+    {
+        printf("Key must be a non-negative integer.\n");
+        return 1;
+    }
+
+    // get plaintext message from user; don't accept EOF as valid input
+    string message;
+    do
+    {
+        message = GetString();
+    }
+    while (message == NULL);
+
+    // encrypt the plaintext in place
+    for (int i = 0; i < strlen(message); i++)
+    {
+        // only encrypt letters, not other chars (e.g., digits)
+        if (isupper(message[i]))
+            message[i] = ((message[i] - 'A' + key) % NUM_LETTERS) + 'A';
+        else if (islower(message[i]))
+            message[i] = ((message[i] - 'a' + key) % NUM_LETTERS) + 'a';
+    }
+
+    // now "message" holds encrypted ciphertext
+    printf("%s\n", message);
+
+    return 0;
+}
+
