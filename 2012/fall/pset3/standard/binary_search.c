d5c229df3bac2a16f1560d1e1385dc64f189ae7c
Nate Hardison <natehardison@gmail.com>
2012-09-28 23:17:15 -0400

    Fixing binary_search problems to match code given in specs

--- 2012/fall/pset3/standard/binary_search.c
+++ 2012/fall/pset3/standard/binary_search.c
@@ -35,7 +35,6 @@ bool search(int needle, int haystack[], int size)
         // gotta look in the lower half of the array
         else
             high = mid - 1;
-
     }
 
     return false;
@@ -43,18 +42,10 @@ bool search(int needle, int haystack[], int size)
 
 int main(void)
 {
-    int numbers[SIZE] = { 4, 8, 15, 16, 23, 42, 50, 108 };
-    
-    while (true)
-    {
-        printf("> ");
-        int n = GetInt();
-        if (n == -1)
-            break;
-        if (search(n, numbers, SIZE))
-            printf("YES\n");
-        else
-            printf("NO\n");
-    }
+    int numbers[SIZE] = { 4, 8, 15, 16, 23, 42, 50, 108 };    
+    printf("> ");
+    int n = GetInt();
+    if (search(n, numbers, SIZE))
+        printf("YES\n");
     return 0;
 }
