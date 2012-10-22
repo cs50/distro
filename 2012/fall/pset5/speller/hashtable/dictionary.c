564f5ade0fcb1e450048ed7e5d4d6a3bf3dc028c
Nate Hardison <natehardison@gmail.com>
2012-10-22 12:53:37 -0400

    Updating hashtable solution: old one was nasty

--- 2012/fall/pset5/speller/hashtable/dictionary.c
+++ 2012/fall/pset5/speller/hashtable/dictionary.c
@@ -1,12 +1,10 @@
 /****************************************************************************
  * dictionary.c
  *
- * Computer Science 50
- * Problem Set 6
+ * Nate Hardison <nate@cs.harvard.edu>
  *
- * Josh Bolduc
- *
- * Implements a dictionary's functionality.
+ * Implements a dictionary's functionality, using a hash table for the 
+ * dictionary.
  ***************************************************************************/
 
 #include <sys/types.h>
@@ -18,132 +16,145 @@
 #include <stdlib.h>
 #include "dictionary.h"
 
+#define HASHTABLE_SIZE 15500
 
-// Global variable to keep track of the dictionary's size
-int g_dict_size = 0;
-
-// Global hashtable
-node *g_table[HASHTABLE_SIZE];
-
-// Pointer to chunk of memory where we'll store all the words in the dict
-char *g_words;
-
-
-/*
- * Returns true if word is in dictionary else false.
+/**
+ * Each node will store a pointer to the word and a pointer to the next node
+ * with the same hash value (or NULL, if no such node follows).
+ *
+ * To be more memory-efficient, we could actually store the character data
+ * inside this struct, as shown here:
+ *
+ * typedef struct node
+ * {
+ *     struct node* next;
+ *     char word[];
+ * }
+ * node;
+ *
+ * char* word = "CS50 rocks!";
+ * node* entry = malloc(sizeof(node) + strlen(word) + 1);
+ * strcpy(entry->word, word);
+ *
+ * Cool beans! We'll do it the "easy" way though for students.
  */
-
-bool
-check(const char *word)
+typedef struct node
 {
-    // make lowercase copy of word in temporary buffer
-    char word_i[LENGTH+1];
-    int len;
-    for (len = 0; word[len] != '\0'; ++len)
-    {
-        word_i[len] = tolower(word[len]);
-    }
-    word_i[len] = '\0';
-    
-    // get the hash value for the lowercase word
-    int hash = hash_word(word_i, NULL);
+    char* word;
+    struct node* next;
+}
+node;
 
-    // see if any word in the dict has this hash value
-    if (g_table[hash] == NULL)
-    {
-        return false;
-    }
+// Global dictionary hashtable
+node* g_table[HASHTABLE_SIZE];
+
+// Global dictionary's size
+int g_table_size = 0;
+
+/**
+ * Returns a hash value for word, in the range [0, HASHTABLE_SIZE - 1]
+ * Lowercases characters before hashing to be case-insensitive
+ * Hash function adapted from:
+ *     http://stackoverflow.com/questions/98153/#98179
+ */
+static int hash_word(const char* word)
+{
+    /* magic numbers from http://www.isthe.com/chongo/tech/comp/fnv/ */
+    size_t InitialFNV = 2166136261U;
+    size_t FNVMultiple = 16777619;
     
-    // see if the word was actually in the dict, looping until
-    // we find it (or NULL)
-    node * ptr = g_table[hash];
-    while (strcmp(ptr->word, word_i) != 0)
+    /* Fowler / Noll / Vo (FNV) Hash */
+    size_t hash = InitialFNV;
+    for (int i = 0; word[i] != '\0'; i++)
     {
-        // NULL means no more words to check, so no match
-        if (ptr->next == NULL)
-        {
-            return false;
-        }
-        ptr = ptr->next;
+        // xor the low 8 bits of the hash
+        hash ^= (tolower(word[i]));
+
+        // multply by the magic number
+        hash *= FNVMultiple;
     }
-    
-    // if we got here without returning false, we must have found the word
-    return true;
+    return (hash % HASHTABLE_SIZE);
 }
 
+/**
+ * Builds a new hash table node with a deep copy of the provided word.
+ * Returns NULL on failure (out of heap memory).
+ */
+static node* build_node(const char* word)
+{
+    // allocate memory for a node
+    node* n = malloc(sizeof(node));
+    if (n == NULL)
+    {
+        return NULL;
+    }
 
-/*
+    // make a persistent heap copy of the word
+    n->word = strdup(word);
+    if (n->word == NULL)
+    {
+        free(n);
+        return NULL;
+    }
+
+    return n;
+}
+
+/**
+ * Returns true if word is in dictionary else false.
+ */
+bool check(const char* word)
+{   
+    // find the bucket in the table where the word should be
+    node* bucket = g_table[hash_word(word)];
+
+    // look at all entries in the bucket to see if the word's there
+    for (node* entry = bucket; entry != NULL; entry = entry->next)
+    {
+        // use strcasecmp since words in dictionary are in lowercase
+        if (strcasecmp(entry->word, word) == 0)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+/**
  * Loads dict into memory.  Returns true if successful else false.
  */
-
-bool
-load(const char *dictionary)
+bool load(const char* dictionary)
 {
     // open the dict and check for errors
-    FILE * fp = fopen(dictionary, "r");
+    FILE* fp = fopen(dictionary, "r");
     if (fp == NULL)
     {
         return false;
     }
     
-    // determine dict size in bytes
-    struct stat stats;
-    if (stat(dictionary, &stats))
-    {
-        fclose(fp);
-        return false;
-    }
-    
-    // allocate enough memory to hold all the words
-    // (we'll assume the dict filesize won't change while we're running)
-    g_words = malloc(stats.st_size);
-    if (g_words == NULL)
-    {
-        fclose(fp);
-        return false;
-    }
-    
-    // initialize all pointers in the hashtable to NULL
-    for (int i = 0; i < HASHTABLE_SIZE; ++i)
-    {
-        g_table[i] = NULL;
-    }
-    
     // loop through words until the end of the file
-    int mem_index = 0;
-    while (!feof(fp))
-    {
-        // scan for a word and, if we find one, store it in our hash table
-        if (fscanf(fp, "%s", &g_words[mem_index]) == 1)
+    char buffer[LENGTH + 1];
+    while (fscanf(fp, "%45s", buffer) == 1)
+    {        
+        node* entry = build_node(buffer);
+        if (entry == NULL)
         {
-            // get both the hash value and string length
-            int len;
-            unsigned int hash = hash_word(&g_words[mem_index], &len);
-            
-            // allocate memory for this word's node
-            node *newword = malloc(sizeof(node));
-            if (newword == NULL)
-            {
-                free(g_words);
-                fclose(fp);
-                return false;
-            }
-
-            // point to the word we just read into memory
-            newword->word = &g_words[mem_index];
-            
-            // point next at the old entry in the hash table at this hash value
-            newword->next = g_table[hash];
-            
-            // make this the new entry in the hash table for this hash value
-            g_table[hash] = newword;
-            
-            // update dictionary size
-            ++g_dict_size;
-            
-            // update offset into our big chunk of memory
-            mem_index += len+1;
+            unload();
+            fclose(fp);
+            return false;
         }
+
+        // hash the word
+        unsigned int hash = hash_word(buffer);
+
+        // point next at the old entry in the hash table at this hash value
+        entry->next = g_table[hash];
+        
+        // make this the new entry in the hash table for this hash value
+        g_table[hash] = entry;
+        
+        // update dictionary size
+        g_table_size++;
     }
     
     // close the dictionary
@@ -154,72 +165,37 @@ load(const char *dictionary)
 }
 
 
-/*
+/**
  * Returns number of words in dictionary if loaded else 0 if not yet loaded.
  */
-
-unsigned int
-size(void)
+unsigned int size(void)
 {
-    return g_dict_size;
+    return g_table_size;
 }
 
 
 /*
  * Unloads dictionary from memory.  Returns true if successful else false.
  */
-
-bool
-unload(void)
+bool unload(void)
 {
-    node *next;
-
     // loop through each hash table entry
-    for (int i = 0; i < HASHTABLE_SIZE; ++i)
+    for (int i = 0; i < HASHTABLE_SIZE; i++)
     {
         // free all the nodes for words with this hash value
-        node *ptr = g_table[i];
-        while (ptr != NULL)
+        node* entry = g_table[i];
+        while (entry != NULL)
         {
-            next = ptr->next;
-            free(ptr);
-            ptr = next;
+            // make sure to save the next pointer prior to freeing!
+            node* next = entry->next;
+
+            // free both the word and the entry
+            free(entry->word);
+            free(entry);
+
+            entry = next;
         }
     }
-    
-    // free the big chunk of memory that held all the words
-    free(g_words);
 
     return true;
-}
-
-
-/*
- * Returns a hash value for word, in the range [0, HASHTABLE_SIZE - 1]
- * If len != NULL, puts the length of word in the address stored in len
- * Hash function adapted from:
- *     http://stackoverflow.com/questions/98153/#98179
- */
- 
-inline
-int
-hash_word(char *word, int *len)
-{
-    /* magic numbers from http://www.isthe.com/chongo/tech/comp/fnv/ */
-    size_t InitialFNV = 2166136261U;
-    size_t FNVMultiple = 16777619;
-    
-    /* Fowler / Noll / Vo (FNV) Hash */
-    size_t hash = InitialFNV, i;
-    for(i = 0; word[i] != '\0'; ++i)
-    {
-        hash = hash ^ (word[i]);    /* xor  the low 8 bits */
-        hash = hash * FNVMultiple;  /* multiply by the magic number */
-    }
-
-    // if requested, return string length
-    if (len)
-        *len = i;
-
-    return (hash % HASHTABLE_SIZE);
 }
--- 2012/fall/pset5/speller/hashtable/dictionary.h
+++ 2012/fall/pset5/speller/hashtable/dictionary.h
@@ -1,10 +1,7 @@
 /****************************************************************************
  * dictionary.h
  *
- * Computer Science 50
- * Problem Set 6
- *
- * Josh Bolduc
+ * Nate Hardison <nate@cs.harvard.edu>
  *
  * Declares a dictionary's functionality.
  ***************************************************************************/
@@ -14,63 +11,28 @@
 
 #include <stdbool.h>
 
-
 // maximum length for a word
 // (e.g., pneumonoultramicroscopicsilicovolcanoconiosis)
 #define LENGTH 45
 
-// size of the hashtable
-#define HASHTABLE_SIZE 155000
-
-
-/*
+/**
  * Returns true if word is in dictionary else false.
  */
-
 bool check(const char *word);
 
-
-/*
+/**
  * Loads dictionary into memory.  Returns true if successful else false.
  */
-
 bool load(const char *dictionary);
 
-
-/*
+/**
  * Returns number of words in dictionary if loaded else 0 if not yet loaded.
  */
-
 unsigned int size(void);
 
-
-/*
+/**
  * Unloads dictionary from memory.  Returns true if successful else false.
  */
-
 bool unload(void);
-
-
-/*
- * Returns a hash value for word, in the range [0, HASHTABLE_SIZE - 1]
- * If len != NULL, puts the length of word in the address stored in len
- * Hash function adapted from:
- *     http://stackoverflow.com/questions/98153/#98179
- */
-int hash_word(char *word, int *len);
-
-
-/*
- * Each node will store a pointer to the word and a pointer to
- * the next node with the same hash value (or NULL, if no such
- * node follows.
- */
-typedef struct _node
-{
-    char *word;
-    struct _node *next; 
-} node;
-
-
 
 #endif // DICTIONARY_H
