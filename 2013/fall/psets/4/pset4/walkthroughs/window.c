f9df55758a7d40472385ed6a732442497bc51f40
David J. Malan <malan@harvard.edu>
2013-10-04 08:44:53 -0400

    adding walkthroughs

---
+++ 2013/fall/psets/4/pset4/walkthroughs/window.c
@@ -0,0 +1,27 @@
+//
+// window.c
+//
+// David J. Malan
+// malan@harvard.edu
+//
+// Draws a window.
+//
+
+// standard libraries
+#include <stdio.h>
+
+// Stanford Portable Library
+#include "gwindow.h"
+
+int main(void)
+{
+    // instantiate window
+    GWindow window = newGWindow(320, 240);
+ 
+    // let's look at it for a while
+    pause(5000);
+
+    // that's all folks
+    closeGWindow(window);
+    return 0;
+}
