86b2174da64610b113154863a4ea0da7284c52f9
David J. Malan <malan@harvard.edu>
2013-10-04 09:20:41 -0400

    more TODOs

---
+++ 2013/fall/psets/4/pset4/walkthroughs/bounce.c
@@ -0,0 +1,28 @@
+//
+// bounce.c
+//
+// David J. Malan
+// malan@harvard.edu
+//
+// TODO
+//
+
+// standard libraries
+#include <stdio.h>
+
+// Stanford Portable Library
+#include "gevents.h"
+#include "gobjects.h"
+#include "gwindow.h"
+
+int main(void)
+{
+    // instantiate window
+    GWindow window = newGWindow(320, 240);
+
+    // bounce forever
+    while (true)
+    {
+        // TODO
+    }
+}
---
+++ 2013/fall/psets/4/pset4/walkthroughs/click.c
@@ -0,0 +1,42 @@
+//
+// click.c
+//
+// David J. Malan
+// malan@harvard.edu
+//
+// Prints cursor's location when mouse's button is clicked.
+//
+// TODO: use window, update Makefile?
+//
+
+// standard libraries
+#include <stdio.h>
+
+// Stanford Portable Library
+#include "gevents.h"
+#include "gobjects.h"
+#include "gwindow.h"
+
+int main(void)
+{
+    // instantiate window
+    GWindow window = newGWindow(320, 240);
+
+    // listen forever
+    while (true)
+    {
+        // check for mouse event
+        GEvent event = getNextEvent(MOUSE_EVENT);
+
+        // if we heard one
+        if (event != NULL)
+        {
+            // if the event was movement
+            if (getEventType(event) == MOUSE_CLICKED)
+            {
+                // print click's coordinates
+                printf("%.0f,%.0f\n", getX(event), getY(event));
+            }
+        }
+    }
+}
