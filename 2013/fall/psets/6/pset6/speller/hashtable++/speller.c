f9a42170cfb90d92c09fd8c942a7f8fabafa1d1d
David J. Malan <malan@harvard.edu>
2013-10-26 13:36:36 -0400

    pset6

--- 2013/fall/psets/6/pset6/speller/hashtable++/speller.c
+++ 2013/fall/psets/6/pset6/speller/hashtable++/speller.c
@@ -16,7 +16,7 @@
 #undef getrusage
 
 // default dictionary
-#define DICTIONARY "/home/cs50/pset5/dictionaries/large"
+#define DICTIONARY "/home/cs50/pset6/dictionaries/large"
 
 // prototype
 double calculate(const struct rusage* b, const struct rusage* a);
