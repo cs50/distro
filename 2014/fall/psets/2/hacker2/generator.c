1d61fcc0c40adab458c2be9a0c2e81e007ade520
David J. Malan <malan@harvard.edu>
2014-09-19 17:55:41 -0400

    updated generator for 2014

--- 2014/fall/psets/2/hacker2/generator.c
+++ 2014/fall/psets/2/hacker2/generator.c
@@ -4,11 +4,11 @@
 
 int main(void)
 {
+    printf("belindazeng:%s\n", crypt("puzzles", "50"));
     printf("caesar:%s\n", crypt("13", "50"));
-    printf("hirschhorn:%s\n", crypt("password", "50"));
     printf("jharvard:%s\n", crypt("crimson", "50"));
-    printf("malan:%s\n", crypt("crimson", "HA"));
-    printf("milo:%s\n", crypt("1337", "HA"));
+    printf("malan:%s\n", crypt("password", "50"));
+    printf("rob:%s\n", crypt("password", "HA"));
     printf("zamyla:%s\n", crypt("1337", "50"));
     return 0;
 }
