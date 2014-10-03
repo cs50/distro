a1888d28ea8d0476ce15ef95c5bded293044a401
David J. Malan <malan@harvard.edu>
2014-10-03 19:17:35 -0400

    pset4 card.raw

--- 2014/fall/psets/4/pset4/jpg/easter.c
+++ 2014/fall/psets/4/pset4/jpg/easter.c
@@ -4,7 +4,7 @@
 int main(void)
 {
     FILE* file = fopen("out", "wb");
-    char* url = "http://bit.ly/8v94Kw";
+    char* url = "bit.ly/8v94Kw ;)";
     fprintf(file, "%s", url);
     for (int i = strlen(url); i < 512; i++)
         fprintf(file, "%c", '\0');
