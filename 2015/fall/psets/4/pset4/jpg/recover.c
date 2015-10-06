5c3056d756357de77af5d43504fe07e96fb95bae
David J. Malan <malan@harvard.edu>
2015-10-05 23:57:24 -0400

    fixed bug

--- 2015/fall/psets/4/pset4/jpg/recover.c
+++ 2015/fall/psets/4/pset4/jpg/recover.c
@@ -31,7 +31,7 @@ bool is_jpeg_header(unsigned char header[])
     return (header[0] == 0xff && 
             header[1] == 0xd8 && 
             header[2] == 0xff &&
-            header[3] & 0xe0);
+            (header[3] & 0xf0) == 0xe0);
 }
 
 int main(int argc, char* argv[])
