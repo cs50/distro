58f9199ee1d342cdf514f22369751dd778efc453
David J. Malan <malan@harvard.edu>
2015-10-30 09:10:02 -0400

    pset7

---
+++ 2015/fall/psets/7/pset7/views/footer.php
@@ -0,0 +1,16 @@
+            </div>
+
+            <div id="bottom">
+                <?php if (!empty($_SESSION["id"])): ?>
+                    <a class="btn btn-default" href="logout.php" role="button">
+                        <span class="glyphicon glyphicon-log-out" aria-hidden="true"></span>
+                        Log Out
+                    </a>
+                <?php endif ?>
+            </div>
+
+        </div>
+
+    </body>
+
+</html>
---
+++ 2015/fall/psets/7/pset7/views/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+
+        <!-- http://getbootstrap.com/ -->
+        <link href="/css/bootstrap.min.css" rel="stylesheet"/>
+
+        <link href="/css/styles.css" rel="stylesheet"/>
+
+        <?php if (isset($title)): ?>
+            <title>C$50 Finance: <?= htmlspecialchars($title) ?></title>
+        <?php else: ?>
+            <title>C$50 Finance</title>
+        <?php endif ?>
+
+        <!-- https://jquery.com/ -->
+        <script src="/js/jquery-1.11.3.min.js"></script>
+
+        <!-- http://getbootstrap.com/ -->
+        <script src="/js/bootstrap.min.js"></script>
+
+        <script src="/js/scripts.js"></script>
+
+    </head>
+
+    <body>
+
+        <div class="container">
+
+            <div id="top">
+                <a href="/"><img alt="C$50 Finance" src="/img/logo.png"/></a>
+            </div>
+
+            <div id="middle">
