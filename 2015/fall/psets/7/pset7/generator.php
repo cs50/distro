7d6cacc13496354058ae3e6e43a3e0f133804617
David J. Malan <malan@harvard.edu>
2015-10-30 20:48:12 -0400

    updated password hash

--- 2015/fall/psets/7/pset7/generator.php
+++ 2015/fall/psets/7/pset7/generator.php
@@ -1,15 +1,15 @@
 #!/bin/env php
 <?php
 
-    printf("INSERT INTO users (username, hash) VALUES('%s', '%s');\n", "andi", crypt("Berkeley", '$1$HA$'));
-    printf("INSERT INTO users (username, hash) VALUES('%s', '%s');\n", "caesar", crypt("13", '$1$50$'));
-    printf("INSERT INTO users (username, hash) VALUES('%s', '%s');\n", "eli", crypt("yale", '$1$50$'));
-    printf("INSERT INTO users (username, hash) VALUES('%s', '%s');\n", "hdan", crypt("boola", '$1$50$'));
-    printf("INSERT INTO users (username, hash) VALUES('%s', '%s');\n", "jason", crypt("Silliman", '$1$50$'));
-    printf("INSERT INTO users (username, hash) VALUES('%s', '%s');\n", "john", crypt("harvard", '$1$50$'));
-    printf("INSERT INTO users (username, hash) VALUES('%s', '%s');\n", "levatich", crypt("PBJ", '$1$50$'));
-    printf("INSERT INTO users (username, hash) VALUES('%s', '%s');\n", "rob", crypt("password", '$1$50$'));
-    printf("INSERT INTO users (username, hash) VALUES('%s', '%s');\n", "skroob", crypt("12345", '$1$50$'));
-    printf("INSERT INTO users (username, hash) VALUES('%s', '%s');\n", "zamyla", crypt("password", '$1$HA$'));
+     printf("INSERT INTO `pset7`.`users` (username, hash) VALUES('%s', '%s');\n", "andi", password_hash("Berkeley", PASSWORD_DEFAULT));
+     printf("INSERT INTO `pset7`.`users` (username, hash) VALUES('%s', '%s');\n", "caesar", password_hash("13", PASSWORD_DEFAULT));
+     printf("INSERT INTO `pset7`.`users` (username, hash) VALUES('%s', '%s');\n", "eli", password_hash("yale", PASSWORD_DEFAULT));
+     printf("INSERT INTO `pset7`.`users` (username, hash) VALUES('%s', '%s');\n", "hdan", password_hash("boola", PASSWORD_DEFAULT));
+     printf("INSERT INTO `pset7`.`users` (username, hash) VALUES('%s', '%s');\n", "jason", password_hash("Silliman", PASSWORD_DEFAULT));
+     printf("INSERT INTO `pset7`.`users` (username, hash) VALUES('%s', '%s');\n", "john", password_hash("harvard", PASSWORD_DEFAULT));
+     printf("INSERT INTO `pset7`.`users` (username, hash) VALUES('%s', '%s');\n", "levatich", password_hash("PBJ", PASSWORD_DEFAULT));
+     printf("INSERT INTO `pset7`.`users` (username, hash) VALUES('%s', '%s');\n", "rob", password_hash("password", PASSWORD_DEFAULT));
+     printf("INSERT INTO `pset7`.`users` (username, hash) VALUES('%s', '%s');\n", "skroob", password_hash("12345", PASSWORD_DEFAULT));
+     printf("INSERT INTO `pset7`.`users` (username, hash) VALUES('%s', '%s');\n", "zamyla", password_hash("password", PASSWORD_DEFAULT));
 
 ?>
--- 2015/fall/psets/7/pset7/pset7.sql
+++ 2015/fall/psets/7/pset7/pset7.sql
@@ -17,13 +17,13 @@ CREATE TABLE IF NOT EXISTS `pset7`.`users` (
 -- Dumping data for table `pset7`.`users`
 --
 
-INSERT INTO `pset7`.`users` (`id`, `username`, `hash`) VALUES(1, 'andi', '$1$HA$VLmVUwflFXcjO7lA5x/Yl/');
-INSERT INTO `pset7`.`users` (`id`, `username`, `hash`) VALUES(2, 'caesar', '$1$50$GHABNWBNE/o4VL7QjmQ6x0');
-INSERT INTO `pset7`.`users` (`id`, `username`, `hash`) VALUES(3, 'eli', '$1$50$bc0ppyn1paKwfvSCB1Vqd0');
-INSERT INTO `pset7`.`users` (`id`, `username`, `hash`) VALUES(4, 'hdan', '$1$50$UMOz1KE6p/PqftaraS.de1');
-INSERT INTO `pset7`.`users` (`id`, `username`, `hash`) VALUES(5, 'jason', '$1$50$Vq4CNGOJzM1vpIazYhsTo1');
-INSERT INTO `pset7`.`users` (`id`, `username`, `hash`) VALUES(6, 'john', '$1$50$xuVbD7p.rTf48ShEZcE/k1');
-INSERT INTO `pset7`.`users` (`id`, `username`, `hash`) VALUES(7, 'levatich', '$1$50$eqqFT6uMdBI.qIVQAoxLo1');
-INSERT INTO `pset7`.`users` (`id`, `username`, `hash`) VALUES(8, 'rob', '$1$50$lJS9HiGK6sphej8c4bnbX.');
-INSERT INTO `pset7`.`users` (`id`, `username`, `hash`) VALUES(9, 'skroob', '$1$50$euBi4ugiJmbpIbvTTfmfI.');
-INSERT INTO `pset7`.`users` (`id`, `username`, `hash`) VALUES(10, 'zamyla', '$1$HA$l5llES7AEaz8ndmSo5Ig41');
+INSERT INTO `pset7`.`users` (username, hash) VALUES('andi', '$2y$10$c.e4DK7pVyLT.stmHxgAleWq4yViMmkwKz3x8XCo4b/u3r8g5OTnS');
+INSERT INTO `pset7`.`users` (username, hash) VALUES('caesar', '$2y$10$0p2dlmu6HnhzEMf9UaUIfuaQP7tFVDMxgFcVs0irhGqhOxt6hJFaa');
+INSERT INTO `pset7`.`users` (username, hash) VALUES('eli', '$2y$10$COO6EnTVrCPCEddZyWeEJeH9qPCwPkCS0jJpusNiru.XpRN6Jf7HW');
+INSERT INTO `pset7`.`users` (username, hash) VALUES('hdan', '$2y$10$o9a4ZoHqVkVHSno6j.k34.wC.qzgeQTBHiwa3rpnLq7j2PlPJHo1G');
+INSERT INTO `pset7`.`users` (username, hash) VALUES('jason', '$2y$10$ci2zwcWLJmSSqyhCnHKUF.AjoysFMvlIb1w4zfmCS7/WaOrmBnLNe');
+INSERT INTO `pset7`.`users` (username, hash) VALUES('john', '$2y$10$dy.LVhWgoxIQHAgfCStWietGdJCPjnNrxKNRs5twGcMrQvAPPIxSy');
+INSERT INTO `pset7`.`users` (username, hash) VALUES('levatich', '$2y$10$fBfk7L/QFiplffZdo6etM.096pt4Oyz2imLSp5s8HUAykdLXaz6MK');
+INSERT INTO `pset7`.`users` (username, hash) VALUES('rob', '$2y$10$3pRWcBbGdAdzdDiVVybKSeFq6C50g80zyPRAxcsh2t5UnwAkG.I.2');
+INSERT INTO `pset7`.`users` (username, hash) VALUES('skroob', '$2y$10$395b7wODm.o2N7W5UZSXXuXwrC0OtFB17w4VjPnCIn/nvv9e4XUQK');
+INSERT INTO `pset7`.`users` (username, hash) VALUES('zamyla', '$2y$10$UOaRF0LGOaeHG4oaEkfO4O7vfI34B1W23WqHr9zCpXL68hfQsS3/e');
