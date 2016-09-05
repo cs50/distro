ea3b44b4f638e0d2f9f826e8251269456543849e
David J. Malan <malan@harvard.edu>
2016-09-05 12:28:53 -0400

    added 3 passwords

---
+++ 2016/fall/problems/crack/.bash_history
@@ -0,0 +1,21 @@
+ls
+make generator
+rm generator.c 
+make generator
+ls
+ls
+rm generator
+make generator
+clang generator.c -lcrypt
+./a.out 
+ls
+vi generator.c .
+vi generator.c
+fg
+ls
+man crypt
+man md5
+man 3 md5
+fg
+ls
+./a.out 
--- 2016/fall/problems/crack/generator.c
+++ 2016/fall/problems/crack/generator.c
@@ -4,14 +4,7 @@
 
 int main(void)
 {
-    printf("andi:%s\n", crypt("Berkeley", "HA"));
-    printf("caesar:%s\n", crypt("13", "50"));
-    printf("eli:%s\n", crypt("yale", "50"));
-    printf("hdan:%s\n", crypt("boola", "50"));
-    printf("jason:%s\n", crypt("Silliman", "50"));
-    printf("john:%s\n", crypt("harvard", "50"));
-    printf("levatich:%s\n", crypt("PBJ", "50"));
-    printf("rob:%s\n", crypt("password", "50"));
+    printf("eyale:%s\n", crypt("blue", "50"));
+    printf("jharvard:%s\n", crypt("crimson", "50"));
     printf("skroob:%s\n", crypt("12345", "50"));
-    printf("zamyla:%s\n", crypt("password", "HA"));
 }
