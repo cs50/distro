d2d5bd5cc4432aba0df7c0f7b9b67ed2b5168cb6
David J. Malan <malan@harvard.edu>
2016-09-09 21:51:17 -0400

    pset2

--- 2016/fall/problems/crack/generator.c
+++ 2016/fall/problems/crack/generator.c
@@ -4,7 +4,14 @@
 
 int main(void)
 {
-    printf("eyale:%s\n", crypt("blue", "50"));
-    printf("jharvard:%s\n", crypt("crimson", "50"));
-    printf("skroob:%s\n", crypt("12345", "50"));
+    printf("andi:%s\n", crypt("hi", "50"));
+    printf("jason:%s\n", crypt("JH", "50"));
+    printf("malan:%s\n", crypt("NOPE", "50"));
+    printf("mzlatkova:%s\n", crypt("ha", "50"));
+    printf("patrick:%s\n", crypt("Yale", "50"));
+    printf("rbowden:%s\n", crypt("rofl", "50"));
+    printf("summer:%s\n", crypt("FTW", "50"));
+    printf("stelios:%s\n", crypt("ABC", "50"));
+    printf("wmartin:%s\n", crypt("haha", "50"));
+    printf("zamyla:%s\n", crypt("lol", "50"));
 }
