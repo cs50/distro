70fb8925b40ff3644486382c5d77a0a081624296
David J. Malan <malan@harvard.edu>
2016-10-30 16:40:40 -0400

    added detection of private accounts

--- helpers.py
+++ helpers.py
@@ -4,7 +4,7 @@ import plotly
 import socket
 
 from twython import Twython
-from twython import TwythonAuthError, TwythonRateLimitError
+from twython import TwythonAuthError, TwythonError, TwythonRateLimitError
 
 def chart(positive, negative, neutral):
     """Return a pie chart for specified sentiments as HTML."""
@@ -48,15 +48,19 @@ def get_user_timeline(screen_name, count=200):
         raise RuntimeError("API_SECRET not set")
 
     # get screen_name's most recent tweets
+    # https://dev.twitter.com/rest/reference/get/users/lookup
     # https://dev.twitter.com/rest/reference/get/statuses/user_timeline
     # https://github.com/ryanmcgrath/twython/blob/master/twython/endpoints.py
     try:
         twitter = Twython(os.environ.get("API_KEY"), os.environ.get("API_SECRET"))
+        user = twitter.lookup_user(screen_name=screen_name)
+        if user[0]["protected"]:
+            return None
         tweets = twitter.get_user_timeline(screen_name=screen_name, count=count)
         return [html.unescape(tweet["text"].replace("\n", " ")) for tweet in tweets]
-    except TwythonAuthError:
+    except TwythonAuthError as e:
         raise RuntimeError("invalid API_KEY and/or API_SECRET") from None
     except TwythonRateLimitError:
         raise RuntimeError("you've hit a rate limit") from None
-    except Exception:
+    except TwythonError:
         return None
