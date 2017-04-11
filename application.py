c6a543dc502168b41f760f9861ee9c0de76c6863
David J. Malan <malan@harvard.edu>
2017-04-10 23:49:40 -0400

    using dedicated temp dir

--- application.py
+++ application.py
@@ -2,7 +2,7 @@ from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session, url_for
 from flask_session import Session
 from passlib.apps import custom_app_context as pwd_context
-from tempfile import gettempdir
+from tempfile import mkdtemp
 
 from helpers import *
 
@@ -22,7 +22,7 @@ if app.config["DEBUG"]:
 app.jinja_env.filters["usd"] = usd
 
 # configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_FILE_DIR"] = gettempdir()
+app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
