a159d686539869a55a7ed09131347d43a898a8ab
David J. Malan <malan@harvard.edu>
2017-10-25 19:14:45 -0400

    handling NotFound

--- application.py
+++ application.py
@@ -1,8 +1,8 @@
 from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
-from passlib.apps import custom_app_context as pwd_context
 from tempfile import mkdtemp
+from werkzeug.exceptions import NotFound
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from helpers import apology, login_required, lookup, usd
