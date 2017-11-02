27d851491102575b2262122b8587cfc3a533d88a
David J. Malan <malan@harvard.edu>
2017-11-01 21:47:36 -0400

    added backup API

--- helpers.py
+++ helpers.py
@@ -48,25 +48,65 @@ def lookup(symbol):
     # query Yahoo for quote
     # http://stackoverflow.com/a/21351911
     try:
+
+        # GET CSV
         url = f"http://download.finance.yahoo.com/d/quotes.csv?f=snl1&s={symbol}"
         webpage = urllib.request.urlopen(url)
+
+        # read CSV
         datareader = csv.reader(webpage.read().decode("utf-8").splitlines())
+
+        # parse first row
         row = next(datareader)
-    except:
-        return None
 
-    # ensure stock exists
+        # ensure stock exists
+        try:
+            price = float(row[2])
+        except:
+            return None
+
+        # return stock's name (as a str), price (as a float), and (uppercased) symbol (as a str)
+        return {
+            "name": row[1],
+            "price": price,
+            "symbol": row[0].upper()
+        }
+
+    except:
+        pass
+
+    # Query Alpha Vantage for quote instead
+    # https://www.alphavantage.co/documentation/
     try:
-        price = float(row[2])
+
+        # GET CSV
+        url = f"https://www.alphavantage.co/query?apikey=NAJXWIA8D6VN6A3K&datatype=csv&function=TIME_SERIES_INTRADAY&interval=1min&symbol={symbol}"
+        webpage = urllib.request.urlopen(url)
+
+        # parse CSV
+        datareader = csv.reader(webpage.read().decode("utf-8").splitlines())
+
+        # ignore first row
+        next(datareader)
+
+        # parse second row
+        row = next(datareader)
+
+        # ensure stock exists
+        try:
+            price = float(row[4])
+        except:
+            return None
+
+        # return stock's name (as a str), price (as a float), and (uppercased) symbol (as a str)
+        return {
+            "name": symbol.upper(), # for backward compatibility with Yahoo
+            "price": price,
+            "symbol": symbol.upper()
+        }
+
     except:
         return None
-
-    # return stock's name (as a str), price (as a float), and (uppercased) symbol (as a str)
-    return {
-        "name": row[1],
-        "price": price,
-        "symbol": row[0].upper()
-    }
 
 
 def usd(value):
